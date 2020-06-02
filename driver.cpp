264c443a4cb451c6ba24f38164a851cae32df976
Kialan Pillay <kialanpillay@gmail.com>
2020-06-02 23:26:10 +0200

    Fit Partial Implementation and Evaluate Implementation

--- driver.cpp
+++ driver.cpp
@@ -11,14 +11,15 @@ using namespace PLLKIA010;
 
 int main(int argc, char *argv[])
 {
-    vector<float> training_labels = {0, 1, 1, 0};
+    vector<float> training_labels = {0, 0, 0, 1};
     vector<Input> training_input = {Input({0, 0}), Input({0, 1}), Input({1, 0}), Input({1, 1})};
     vector<Input> test = {Input({0, 0}), Input({0, 1}), Input({1, 0}), Input({1, 1})};
     Training train(training_labels, training_input);
     float learning_rate = 0.1;
-    int layers = 2;
+    int layers = 1;
     NeuralNetwork nn(train, test, learning_rate, layers);
     nn.fit();
+    nn.evaluate();
     //ofstream out("output.txt");
     //out.close();
     return 0;
--- neural_network.cpp
+++ neural_network.cpp
@@ -12,43 +12,94 @@ NeuralNetwork::NeuralNetwork(void) {}
 
 NeuralNetwork::NeuralNetwork(const Training &train, const std::vector<Input> &test, const float r, const int l) : training_data(train), test_data(test), learning_rate(r), layers(l)
 {
-    
+
     for (int i = 0; i < l; ++i)
     {
         std::vector<Perceptron> layer;
         model.push_back(layer);
         std::vector<float> weights;
+        srand(static_cast<unsigned>(time(0)));
+
         for (int w = 0; w < 2; ++w)
         {
             float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
             weights.push_back(r);
         }
-        if (i != 0)
+
+        if (i == l - 1)
         {
-            model[i].push_back(Perceptron("step", weights));
-            model[i].push_back(Perceptron("step", weights)); //2-Node Hidden Layer
+            model[i].push_back(Perceptron("step", weights)); //Output Layer
         }
         else
         {
-            model[i].push_back(Perceptron("step", weights)); //Output Layer
+            model[i].push_back(Perceptron("step", weights));
+            model[i].push_back(Perceptron("step", weights)); //2-Node Hidden Layer
         }
     }
 }
 
 void NeuralNetwork::fit(void)
 {
-    for (int m = 0; m < training_data.train_input.size(); ++m)
+    for (int k = 0; k < 10; ++k)
     {
-        std::vector<float> x = training_data.train_input[m].x;
-        for (auto &perceptron : model[0])
+        //std::cout << "Iteration " << (k + 1) << std::endl;
+        for (int m = 0; m < training_data.train_input.size(); ++m)
         {
+            std::vector<float> x = training_data.train_input[m].x;
+            for (auto &perceptron : model[0])
+            {
+                std::vector<float> linear;
+                std::transform(perceptron.weights.begin(), perceptron.weights.end(),
+                               x.begin(), std::back_inserter(linear),
+                               std::multiplies<float>());
+                float sum = std::accumulate(linear.begin(), linear.end(), 0.0);
 
-            std::vector<float> linear;
-            std::transform(perceptron.weights.begin(), perceptron.weights.end(),
-                           x.begin(), std::back_inserter(linear),
-                           std::multiplies<float>());
-            float sum = std::accumulate(linear.begin(), linear.end(), 0);
-            std::cout << sum;
+                if (perceptron.activation == "step")
+                {
+                    if (sum > 1.5)
+                    {
+                        perceptron.output = 1;
+                    }
+                    else
+                    {
+                        perceptron.output = 0;
+                    }
+                }
+                float t = training_data.train_labels[m];
+                float o = perceptron.output;
+                float rate = learning_rate;
+
+                std::vector<float> delta;
+                std::transform(x.begin(), x.end(), std::back_inserter(delta), [rate, t, o](float x) { return rate * (t - o) * x; });
+                std::transform(perceptron.weights.begin(), perceptron.weights.end(),
+                               delta.begin(), perceptron.weights.begin(),
+                               std::plus<float>());
+            }
+        }
+        for (auto w : model[0][0].weights)
+        {
+            //std::cout << w << " ";
+        }
+        //std::cout << std::endl << std::endl;
+    }
+}
+
+void NeuralNetwork::evaluate(void)
+{
+    for (auto i : test_data)
+    {
+        std::vector<float> linear;
+        std::transform(model[0][0].weights.begin(), model[0][0].weights.end(),
+                       i.x.begin(), std::back_inserter(linear),
+                       std::multiplies<float>());
+        float sum = std::accumulate(linear.begin(), linear.end(), 0.0);
+        if (sum > 1.5)
+        {
+            std::cout << "Prediction: " << 1;
+        }
+        else
+        {
+            std::cout << "Prediction: " << 0 << std::endl;
         }
     }
 }
