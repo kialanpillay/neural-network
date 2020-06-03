816a45b4cc3e01b51b4ee25b4cb935b3bc0bcce4
Kialan Pillay <kialanpillay@gmail.com>
2020-06-03 21:50:54 +0200

    Change Test Case

--- driver.cpp
+++ driver.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     vector<float> training_labels = {0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0};
     vector<float> test_labels = {0, 1, 1, 0, 0, 1, 1, 0};
     vector<Input> training_input = {Input({0, 0}), Input({0, 1}), Input({1, 0}), Input({1, 1}), Input({0, 0}), Input({0, 1}), Input({1, 0}), Input({1, 1}), Input({0, 0}), Input({0, 1}), Input({1, 0}), Input({1, 1})};
-    vector<Input> test_input = {Input({0, 0}), Input({0, 1}), Input({1, 0}), Input({1, 1}), Input({0, 0.2}), Input({0, 1.1}), Input({0.9, 0.1}), Input({0.9, 1})};
+    vector<Input> test_input = {Input({0, 0}), Input({0, 1}), Input({1, 0}), Input({1, 1}), Input({0, 0}), Input({0, 1.1}), Input({0.9, 0.1}), Input({0.9, 1})};
     Data train_xor(training_labels, training_input);
     Data test_xor(test_labels, test_input);
     float learning_rate = 0.1;
